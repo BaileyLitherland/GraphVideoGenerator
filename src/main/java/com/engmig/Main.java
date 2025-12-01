22a5f2caa3504859589eb4a5483b709d4aef3dfa
Bailey Litherland <Baileylitherland02@gmail.com>
2025-12-01 20:58:52 +1100

    Uses the scene builder class to build a scene. Currently just a blank canvas that takes up the whole screen.

--- src/main/java/com/engmig/Main.java
+++ src/main/java/com/engmig/Main.java
@@ -1,5 +1,29 @@
 package com.engmig;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Region;
+import javafx.stage.Stage;
+
 public class Main extends Application {
 
+    @Override
+    public void start(Stage primaryStage){
+
+        ScreenBuilder screenBuilder = new ScreenBuilder();
+        Region screenRoot = screenBuilder.build();
+        Scene scene = new Scene(screenRoot);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void stop(){
+        System.exit(0);
+    }
 }
