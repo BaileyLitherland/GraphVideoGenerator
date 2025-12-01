62b81dead188e3429f848210f07e4e967ed7e4f0
Bailey Litherland <Baileylitherland02@gmail.com>
2025-12-01 21:33:51 +1100

    Added a timer to main

--- src/main/java/com/engmig/GraphicsHandler.java
+++ src/main/java/com/engmig/GraphicsHandler.java
@@ -4,8 +4,8 @@ import javafx.scene.canvas.GraphicsContext;
 
 public class GraphicsHandler {
 
-    static GraphicsHandler ghInstance;
-    static GraphicsContext gc;
+    private static GraphicsHandler ghInstance;
+    private static GraphicsContext gc;
 
     public GraphicsHandler(GraphicsContext graphicContext){
          gc = graphicContext;
@@ -15,16 +15,23 @@ public class GraphicsHandler {
 
     }
     // Static Factory Method
-    public GraphicsHandler getInstance(){
+    public static GraphicsHandler getInstance(){
         if (ghInstance != null){
             ghInstance = new GraphicsHandler();
         }
         return ghInstance;
     }
 
-    public GraphicsHandler newGraphicsHandler(GraphicsContext graphicsContext){
+    public static GraphicsHandler newGraphicsHandler(GraphicsContext graphicsContext){
         ghInstance = new GraphicsHandler(graphicsContext);
         return ghInstance;
     }
 
+    public static GraphicsContext getGc() {
+        return gc;
+    }
+
+    public void update(Graph graph){
+        // In here update the canvas based on the graph
+    }
 }
--- src/main/java/com/engmig/Main.java
+++ src/main/java/com/engmig/Main.java
@@ -1,13 +1,17 @@
 package com.engmig;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.scene.layout.Region;
 import javafx.stage.Stage;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Main extends Application {
 
-
+    GraphicsHandler gh;
     @Override
     public void start(Stage primaryStage){
 
@@ -15,12 +19,26 @@ public class Main extends Application {
         Region screenRoot = screenBuilder.build();
         Scene scene = new Scene(screenRoot);
 
+        gh = GraphicsHandler.newGraphicsHandler(screenBuilder.getCanvas().getGraphicsContext2D());
+
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
-    public static void main(String[] args) {
-        launch();
+    private void startTimer(){
+        Timer timer = new Timer();
+
+        TimerTask task = new TimerTask(){
+            @Override
+            public void run(){
+                // This part runs on background thread
+
+                Platform.runLater(() ->{
+                    // This runs on Application Thread. Use to update UI
+
+                });
+            }
+        };
     }
 
     @Override
