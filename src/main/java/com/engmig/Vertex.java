7a8cf4b76abf9b8b225ce1fce19535324af26a70
Bailey Litherland <Baileylitherland02@gmail.com>
2025-12-02 22:10:27 +1100

    Added position to vertex and a draw function

--- src/main/java/com/engmig/Vertex.java
+++ src/main/java/com/engmig/Vertex.java
@@ -1,4 +1,17 @@
 package com.engmig;
 
+import javafx.scene.canvas.GraphicsContext;
+
+import javax.vecmath.Vector3d;
+
 public class Vertex {
+    Vector3d pos;
+
+    public Vertex(double x, double y){
+        pos = new Vector3d(x,y,0);
+    }
+
+    public void draw(GraphicsContext gc){
+        gc.fillOval(pos.getX()-5, pos.getY()-5, 10,10);
+    }
 }
